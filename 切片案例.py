9edff97dc6d04e63011207509776940bb32edc4e
zsc_data <wangyi2201220@163.com>
2023-05-04 16:43:03 +0800

    1

---
+++ 切片案例.py
@@ -0,0 +1,10 @@
+# _*_ coding : utf-8 _*_
+# @Time : 2023/5/4 16:26
+# @Author : 501036554@qq.com
+# @File : 字符串案例
+# @Project : python-xuexi
+
+my = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# 对序列进行切片操作：截取指定的
+my1 = my[0:9:2]
+print(my1)
