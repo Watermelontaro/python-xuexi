3ea1810d45f8916f1e752c814da92ae180fe4312
zsc_data <wangyi2201220@163.com>
2023-05-04 18:36:56 +0800

    1

---
+++ 集合案例.py
@@ -0,0 +1,11 @@
+# _*_ coding : utf-8 _*_
+# @Time : 2023/5/4 18:32
+# @Author : 501036554@qq.com
+# @File : 集合案例
+# @Project : python-xuexi
+
+my_list = ["黑马程序员", "传智播客", "黑马程序员", "传智播客", "itheuma", "itheima", "itcast", "best"]
+se = set()
+for i in my_list:
+    se.add(i)
+print(se)
--- 集合的方法.py
+++ 集合的方法.py
@@ -3,3 +3,22 @@
 # @Author : 501036554@qq.com
 # @File : 集合的方法
 # @Project : python-xuexi
+pot = {6, 8, 5, 1, 2}
+log = {1, 2, 3, 4}
+# 取出pot中有，log中没有的元素 有返回值
+se = pot.difference(log)
+print(se)
+# 删除掉pot中有，log中没有的元素 无返回值
+pot.difference_update(log)
+print(se)
+print(pot)
+# 两个集合合成一个集合
+se = pot.union(log)
+print(pot)
+print(log)
+print(se)
+# 统计集合的元素数量
+print(len(pot))
+# 集合的遍历
+for i in log:
+    print(i)
