f1f152920ce29a8cc9b1cbe423d524740fe41c7c
zsc_data <wangyi2201220@163.com>
2023-05-06 15:52:53 +0800

    文件综合案例

--- 异常&模块&包/异常.py
+++ 异常&模块&包/异常.py
@@ -3,3 +3,11 @@
 # @Author : 501036554@qq.com
 # @File : 异常
 # @Project : python-xuexi
+
+# 异常
+# try:可能出现异常的代码
+try:
+    print("1", open("D:\mysql\word.txt", "r", encoding="utf-8"))
+# except:出现异常后执行的代码
+except:
+    print("2", open("C:\mysql\word.txt", "r", encoding="utf-8"))
