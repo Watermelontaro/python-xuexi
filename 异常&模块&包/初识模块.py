0781d0ad8f49f42fe12d9ac326fa5fd8ceb01067
zsc_data <wangyi2201220@163.com>
2023-05-06 17:39:56 +0800

    1

---
+++ 异常&模块&包/初识模块.py
@@ -0,0 +1,18 @@
+# _*_ coding : utf-8 _*_
+# @Time : 2023/5/6 17:29
+# @Author : 501036554@qq.com
+# @File : 初识模块
+# @Project : python-xuexi
+
+# 导入python自带的模块time
+import time
+# 导入python自带的模块time中的sleep
+from time import sleep
+
+print("你好")
+# time模块中让程序延时3秒钟
+# 第一种导入方式的使用
+time.sleep(3)
+# 第二种导入方式的使用
+sleep(3)
+print("hello")
