b30ec9ab0dbbc9ec89a878973a836a95ce1ab3e8
zsc_data <wangyi2201220@163.com>
2023-05-06 17:02:13 +0800

    1

---
+++ 异常&模块&包/异常的else和finally.py
@@ -0,0 +1,5 @@
+# _*_ coding : utf-8 _*_
+# @Time : 2023/5/6 16:58
+# @Author : 501036554@qq.com
+# @File : 异常的else和finally
+# @Project : python-xuexi
---
+++ 异常&模块&包/捕获所有的异常.py
@@ -0,0 +1,10 @@
+# _*_ coding : utf-8 _*_
+# @Time : 2023/5/6 16:59
+# @Author : 501036554@qq.com
+# @File : 捕获所有的异常
+# @Project : python-xuexi
+# Exception 能够捕获所有的异常 "except："是它的简写
+try:
+    print(n)
+except Exception as e:
+    print(e)
