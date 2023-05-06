7d4422c09fd5849c1185fe16ee94327996945c7f
zsc_data <wangyi2201220@163.com>
2023-05-06 15:41:28 +0800

    文件综合案例

---
+++ 异常&模块&包/异常.py
@@ -0,0 +1,5 @@
+# _*_ coding : utf-8 _*_
+# @Time : 2023/5/6 15:41
+# @Author : 501036554@qq.com
+# @File : 异常
+# @Project : python-xuexi
--- 文件操作/文件综合案例.py
+++ 文件操作/文件综合案例.py
@@ -3,3 +3,15 @@
 # @Author : 501036554@qq.com
 # @File : 文件综合案例
 # @Project : python-xuexi
+i = open("C:\mysql\\bill.txt", "r", encoding="utf-8")
+for j in i:
+    m = j.replace("\n", " ")
+    m = m.strip()
+    print(m)
+    a, b = m[-2], m[-1]
+    a = a + b
+    print(a)
+    if a != "测试":
+        with open("C:\mysql\\bill.txt.bak", "a", encoding="utf-8") as l:
+            l.write(j)
+            print("正式：", j)
