7f14ec4c4150ef6c6bfa651ece742e1c600dd1de
zsc_data <wangyi2201220@163.com>
2023-05-08 08:38:36 +0800

    1

---
+++ 异常&模块&包/自定义模块.py
@@ -0,0 +1,12 @@
+# _*_ coding : utf-8 _*_
+# @Time : 2023/5/8 8:27
+# @Author : 501036554@qq.com
+# @File : 自定义模块
+# @Project : python-xuexi
+
+def test(a, b):
+    print(a + b)
+
+
+if __name__ == '__main__':
+    test(1, 3)
---
+++ 异常&模块&包/自定义模块1.py
@@ -0,0 +1,11 @@
+# _*_ coding : utf-8 _*_
+# @Time : 2023/5/8 8:29
+# @Author : 501036554@qq.com
+# @File : 自定义模块1
+# @Project : python-xuexi
+
+def test(a, b):
+    print(a - b)
+
+
+test(5, 20)
