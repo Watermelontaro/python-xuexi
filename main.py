198e9f74bcdfee06eabcd893f13c5031a42a86a8
zscpc <wangyi2201220@163.com>
2023-05-11 15:07:52 +0800

    Changes

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-name = "传智播客"
-stock_code = "003032"
-print(f"公司：{name}\n股票代码：{stock_code}")
-stock_price = float(input("输入当前的股价："))
-stock_price_daily_growth_factor = 1.2
-growth_days = 7
-finally_stock_price = stock_price * stock_price_daily_growth_factor ** growth_days
-print(f"公司：{name}，股票代码：{stock_code}，当前股价：{stock_price}")
-print("每日增长系数：%.1f，经过%d天的增长后，股价达到：%.2f" % (
-    stock_price_daily_growth_factor, growth_days, finally_stock_price))
-# 基本语句的使用
+# 这是一个示例 Python 脚本。
+
+# 按 Shift+F10 执行或将其替换为您的代码。
+# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+
+
+def print_hi(name):
+    # 在下面的代码行中使用断点来调试脚本。
+    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+
+
+# 按间距中的绿色按钮以运行脚本。
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
