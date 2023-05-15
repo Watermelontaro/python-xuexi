0b22470ea2a8f5d2dc9ad65bc27b51695e9ea067
zscpc <wangyi2201220@163.com>
2023-05-15 17:05:10 +0800

    Changes

--- 数据可视化/pyecharts/折线图.py
+++ 数据可视化/pyecharts/折线图.py
@@ -1,7 +1,18 @@
 from pyecharts.charts import Line
+from pyecharts.options import TitleOpts, ToolboxOpts, VisualMapOpts, LegendOpts
 
 line = Line()
-print(line)
+# x轴
 line.add_xaxis(["中国", "美国", "英国"])
+# y轴
 line.add_yaxis("GDP", [30, 20, 10])
+# 全局控制项
+line.set_global_opts(
+    title_opts=TitleOpts(title="GDP工具", pos_left="center", pos_bottom="1%"),
+    toolbox_opts=ToolboxOpts(is_show=True),
+    legend_opts=LegendOpts(is_show=True),
+    visualmap_opts=VisualMapOpts(is_show=True)
+
+)
+# 生成图像
 line.render()
