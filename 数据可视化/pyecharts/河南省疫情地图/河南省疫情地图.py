de4bd3350a617070ed4a8464fb1adecb4ed9c477
zscpc <wangyi2201220@163.com>
2023-05-17 15:52:12 +0800

    Changes

--- 数据可视化/pyecharts/河南省疫情地图/河南省疫情地图.py
+++ 数据可视化/pyecharts/河南省疫情地图/河南省疫情地图.py
@@ -18,16 +18,20 @@ for i in data:
     data_list.append((data_name, data_total))
 print(data_list)
 map = Map()
+map.add("河南省疫情数据可视化地图", data_list, "河南")
 map.set_global_opts(
     title_opts=TitleOpts(title="河南省"),
     visualmap_opts=VisualMapOpts(
         is_show=True,
         is_piecewise=True,
         pieces=[
-            {min: 1, max: 10, "liable": "1~10", "color": ""},
+            {"min": 1, "max": 9, "label": "1-9人", "color": "#ccffff"},
+            {"min": 10, "max": 50, "label": "10-50人", "color": "#ffff99"},
+            {"min": 51, "max": 100, "label": "51-100人", "color": "#ff9966"},
+            {"min": 101, "max": 150, "label": "101-150人", "color": "#ff6666"},
+            {"min": 151, "label": "151+人", "color": "#ff3333"}
         ]
     )
 )
-map.add("河南省疫情数据可视化地图", data_list, "河南")
 
 map.render("河南省疫情地图.html")
